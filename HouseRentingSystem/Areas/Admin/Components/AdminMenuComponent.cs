528ce3d156531ad842af52da9acf46f3f2705f05
MVM)7 <mvmanchev07@gmail.com>
2025-04-16 15:06:09 +0300

    Controllers and AdminMenuComponent added

---
+++ HouseRentingSystem/Areas/Admin/Components/AdminMenuComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HouseRentingSystem.Components
+{
+    public class AdminMenuComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult<IViewComponentResult>(View());
+        }
+    }
+}
